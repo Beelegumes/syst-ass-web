58dd75e81e99d7bcde0b27c20e5c5fdd6bdea0a1
Barbara <barbaraannemackinder@gmail.com>
2017-05-21 14:08:46 +0100

    Upgrade WP in syst-ass-web to new version 4.7.5

--- www.systass.org/wwwroot/wordpress/wp-config.php
+++ www.systass.org/wwwroot/wordpress/wp-config.php
@@ -16,18 +16,24 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+// commented out settings are for local development testing
+//define('DB_NAME', 'wordpress');
 define('DB_NAME', 'wp_syst_2013');
 
 /** MySQL database username */
+//define('DB_USER', 'systass');
 define('DB_USER', 'syst_usr_wp');
 
 /** MySQL database password */
+//define('DB_PASSWORD', 'm73)o4GUrmm%c286s2');
 define('DB_PASSWORD', '5ruGaswePr');
 
 /** MySQL hostname */
+//define('DB_HOST', 'localhost');
 define('DB_HOST', '81.27.106.131');
 
 /** Database Charset to use in creating database tables. */
+//define('DB_CHARSET', 'utf8mb4');
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
@@ -42,6 +48,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
+//define('AUTH_KEY',         'nWeQu~6@/#O^>3m}<)o3S^z,R-k5duZgRYh8~1IwM(b.J}H,%=Btxug[0a&L}qY`');
+//define('SECURE_AUTH_KEY',  'erUD8!qqsw:QS$K~53W;&{bE1*s9$!sL, TzNYxN 76nCeb_l9Q=r)+EArBYhB)=');
+//define('LOGGED_IN_KEY',    '8CQVv{llm`+c+x[Ba*kG&/UfnFw?1B#Iyagig5X(0b^,G+TQSIqVij7A:3+tDSW3');
+//define('NONCE_KEY',        '6+b6nkc8J!J$km9ZET4)3QpASN.b<nE<tmjmOP*!eJL^|s!9MQ}<` IO&q_%a P{');
+//define('AUTH_SALT',        '5@=$pe/JgDkj8%mKi9ju(fhp]+=al6u~AT*Ayo$- @OmD33GFWNM(9VC? 8;e90g');
+//define('SECURE_AUTH_SALT', '}T{,5z&.mzjK|!z-S.ms}& jf[+9T9lrDY{$=OSS:FQ~>f8D*Kqr?w%MJJtJmBRd');
+//define('LOGGED_IN_SALT',   'Z92!#dcGKYYw]rO?I{/ismNJCTSx7fk`9f3hD[RRzlwFF:$[@{uvr>kNlPku^4or');
+//define('NONCE_SALT',       'hG!)sDYp}{QRk^#k+v&jXLU.(^&p!@P~.]<^t[FPiKZgpAFU6THzV*b1==M=f6Qo');
 define('AUTH_KEY',         ';_Zp1dfDXFQHy*}.ajIK13m$+c)<}/:{N@C`8V})iW$1e().3H#T_OT;h~H;c[]:');
 define('SECURE_AUTH_KEY',  'L2aN;Jz0xdeSUt )8t`OC]D}vu5Zt]N79><@C3yy:&u)7:Ti|p)&CF;0P9hASH=Y');
 define('LOGGED_IN_KEY',    ',Ws=7?C{op1n4xBDa}ZOx[h!uF0C[Y4eToE)xn8pr@?&.fzL;z{6oQ9z*y>P.:7)');
@@ -77,6 +91,11 @@ define('WPLANG', '');
  * Change this to true to enable the display of notices during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
 
